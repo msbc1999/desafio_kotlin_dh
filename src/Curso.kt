4ca0af9db58f8fb42a2208a2e8efca0811f5a965
Mateus Câmara <mc.email.1999@gmail.com>
2020-09-25 21:50:35 -0300

    Implementação - Parte E Adição dos atributos necessários na classe Curso.kt

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,4 +1,6 @@
-class Curso(var nome: String, var codigoCurso: Int) {
+class Curso(var nome: String, var codigoCurso: Int, var professorTitular: ProfessorTitular, var professorAdjunto: ProfessorAdjunto, var maxAlunos: Int) {
+
+    val alunosMatriculados = mutableListOf<Aluno>()
 
     override fun equals(other: Any?): Boolean = if (other is Curso) other.codigoCurso == codigoCurso else false
 
