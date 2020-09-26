1a287c2ccee743ed44fc5fce03401ee2684fd5e0
Mateus Câmara <mc.email.1999@gmail.com>
2020-09-25 23:25:07 -0300

    Implementação - Parte H Funções para adicionar e remover aluno na classe Curso.kt implementadas

--- src/Curso.kt
+++ src/Curso.kt
@@ -2,6 +2,14 @@ class Curso(var nome: String, var codigoCurso: Int, var professorTitular: Profes
 
     val alunosMatriculados = mutableListOf<Aluno>()
 
+    fun adicionarAluno(aluno: Aluno): Boolean {
+        if (alunosMatriculados.size >= maxAlunos) return false
+        alunosMatriculados.add(aluno)
+        return true
+    }
+
+    fun excluirAluno(aluno: Aluno) = alunosMatriculados.removeIf { it.equals(aluno) }
+
     override fun equals(other: Any?): Boolean = if (other is Curso) other.codigoCurso == codigoCurso else false
 
 }
