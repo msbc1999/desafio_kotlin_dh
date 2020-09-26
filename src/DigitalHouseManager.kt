28690ea0b60946ea86ba42d7e103ab2b55daa14c
Mateus Câmara <mc.email.1999@gmail.com>
2020-09-25 23:08:05 -0300

    Implementação - Parte G Criação da classe DigitalHouseManager.kt com seus atributos

---
+++ src/DigitalHouseManager.kt
@@ -0,0 +1,6 @@
+class DigitalHouseManager {
+    val alunos = mutableListOf<Aluno>()
+    val professores = mutableListOf<Professor>()
+    val cursos = mutableListOf<Curso>()
+    val matriculas = mutableListOf<Matricula>()
+}
